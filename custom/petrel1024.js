a6fdbf6cc7bfc1c478efc92a21f6db7f21e74db7
petrel <petrel1024@163.com>
2020-09-22 15:23:55 +0800

    Site updated: 2020-09-22 15:23:54

--- custom/petrel1024.js
+++ custom/petrel1024.js
@@ -1,8 +1,8 @@
 // 如果不显示侧边栏，修改描述信息的位置
-var postBg = document.getElementsByClassName("post-bg");
-var postInfo = document.getElementById("post-info");
-if (postBg.length != 0) {
-  if (postBg[0].previousSibling.id != "sidebar") {
-    postInfo.style.padding = "0 16%";
-  }
-}
+// var postBg = document.getElementsByClassName("post-bg");
+// var postInfo = document.getElementById("post-info");
+// if (postBg.length != 0) {
+//   if (postBg[0].previousSibling.id != "sidebar") {
+//     postInfo.style.padding = "0 16%";
+//   }
+// }
